6de1ddaee35e2b55ee682ee13206c681e7657ae0
Thiago Leite <thiagoflosino@gmail.com>
2021-09-19 18:13:24 -0300

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:rural_de_bolso/screens/Login.dart';
 import 'package:rural_de_bolso/screens/MateriaScreen.dart';
-import 'package:rural_de_bolso/screens/NotificacaoPage.dart';
 import 'package:rural_de_bolso/screens/dashboard.dart';
 import 'package:rural_de_bolso/utils/HttpConnection.dart';
 import 'package:rural_de_bolso/utils/app_router.dart';
+import 'package:android_alarm_manager_plus/android_alarm_manager_plus.dart';
 
-void main() {
+void main() async {
   runApp(RuralDeBolso());
+  // await AndroidAlarmManager.initialize();
+  // await AndroidAlarmManager.periodic(
+  //     const Duration(minutes: 1), 0, atualizaDados());
 }
 
 class RuralDeBolso extends StatelessWidget {
@@ -20,7 +23,7 @@ class RuralDeBolso extends StatelessWidget {
       theme: ThemeData(
           primarySwatch: Colors.green, accentColor: Colors.greenAccent),
       initialRoute: AppRouter.LOGIN,
-//          appController.instance.isLogged ? AppRouter.HOME : AppRouter.LOGIN,
+      // initialRoute: appController.instance.isLogged ? AppRouter.HOME : AppRouter.LOGIN,
       routes: {
         AppRouter.LOGIN: (context) => Login(),
         AppRouter.HOME: (context) => Dashboard(),
@@ -30,6 +33,14 @@ class RuralDeBolso extends StatelessWidget {
   }
 }
 
+// atualizaDados() {
+//   LandingPage.instance.extraiInformacaoesLanding().then((value) => {
+//         ScaffoldMessenger.of(context).hideCurrentSnackBar(),
+//         Navigator.of(context)
+//             .pushReplacementNamed(AppRouter.HOME, arguments: value),
+//             print("executou")
+//       });
+// }
 // class Splash extends StatelessWidget {
 //   @override
 //   Widget build(BuildContext context) {
---
+++ lib/store/AuthStore.g.dart
@@ -0,0 +1,132 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'AuthStore.dart';
+
+// **************************************************************************
+// StoreGenerator
+// **************************************************************************
+
+// ignore_for_file: non_constant_identifier_names, unnecessary_brace_in_string_interps, unnecessary_lambdas, prefer_expression_function_bodies, lines_longer_than_80_chars, avoid_as, avoid_annotating_with_dynamic
+
+mixin _$AuthStore on _AuthStoreBase, Store {
+  Computed<bool>? _$isValidComputed;
+
+  @override
+  bool get isValid => (_$isValidComputed ??=
+          Computed<bool>(() => super.isValid, name: '_AuthStoreBase.isValid'))
+      .value;
+
+  final _$usernameAtom = Atom(name: '_AuthStoreBase.username');
+
+  @override
+  String get username {
+    _$usernameAtom.reportRead();
+    return super.username;
+  }
+
+  @override
+  set username(String value) {
+    _$usernameAtom.reportWrite(value, super.username, () {
+      super.username = value;
+    });
+  }
+
+  final _$passwordAtom = Atom(name: '_AuthStoreBase.password');
+
+  @override
+  String get password {
+    _$passwordAtom.reportRead();
+    return super.password;
+  }
+
+  @override
+  set password(String value) {
+    _$passwordAtom.reportWrite(value, super.password, () {
+      super.password = value;
+    });
+  }
+
+  final _$isLoggedAtom = Atom(name: '_AuthStoreBase.isLogged');
+
+  @override
+  bool get isLogged {
+    _$isLoggedAtom.reportRead();
+    return super.isLogged;
+  }
+
+  @override
+  set isLogged(bool value) {
+    _$isLoggedAtom.reportWrite(value, super.isLogged, () {
+      super.isLogged = value;
+    });
+  }
+
+  final _$loadingAtom = Atom(name: '_AuthStoreBase.loading');
+
+  @override
+  bool get loading {
+    _$loadingAtom.reportRead();
+    return super.loading;
+  }
+
+  @override
+  set loading(bool value) {
+    _$loadingAtom.reportWrite(value, super.loading, () {
+      super.loading = value;
+    });
+  }
+
+  final _$loginAsyncAction = AsyncAction('_AuthStoreBase.login');
+
+  @override
+  Future<void> login() {
+    return _$loginAsyncAction.run(() => super.login());
+  }
+
+  final _$_AuthStoreBaseActionController =
+      ActionController(name: '_AuthStoreBase');
+
+  @override
+  void setUsername(String _username) {
+    final _$actionInfo = _$_AuthStoreBaseActionController.startAction(
+        name: '_AuthStoreBase.setUsername');
+    try {
+      return super.setUsername(_username);
+    } finally {
+      _$_AuthStoreBaseActionController.endAction(_$actionInfo);
+    }
+  }
+
+  @override
+  void setPassword(String _password) {
+    final _$actionInfo = _$_AuthStoreBaseActionController.startAction(
+        name: '_AuthStoreBase.setPassword');
+    try {
+      return super.setPassword(_password);
+    } finally {
+      _$_AuthStoreBaseActionController.endAction(_$actionInfo);
+    }
+  }
+
+  @override
+  void setIsLogged(bool _isLogged) {
+    final _$actionInfo = _$_AuthStoreBaseActionController.startAction(
+        name: '_AuthStoreBase.setIsLogged');
+    try {
+      return super.setIsLogged(_isLogged);
+    } finally {
+      _$_AuthStoreBaseActionController.endAction(_$actionInfo);
+    }
+  }
+
+  @override
+  String toString() {
+    return '''
+username: ${username},
+password: ${password},
+isLogged: ${isLogged},
+loading: ${loading},
+isValid: ${isValid}
+    ''';
+  }
+}
