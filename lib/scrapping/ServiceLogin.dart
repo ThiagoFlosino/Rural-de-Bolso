1092c5ede3e4efc962ca2eb2103aa172eaea6c78
Thiago Leite <thiagoflosino@gmail.com>
2021-09-19 21:50:32 -0300

    Revert "commit"
    This reverts commit 6de1ddaee35e2b55ee682ee13206c681e7657ae0.

--- lib/scrapping/ServiceLogin.dart
+++ lib/scrapping/ServiceLogin.dart
@@ -1,14 +1,10 @@
-import 'dart:developer';
-
 import 'package:dio/dio.dart';
 import 'package:rural_de_bolso/utils/HttpConnection.dart';
 import 'package:rural_de_bolso/utils/UserStorage.dart';
 import 'package:rural_de_bolso/utils/appController.dart';
-import 'package:html/parser.dart' show parse;
 
 class ServiceLogin {
   static Future<bool> doLogin(user, pass) async {
-    // TODO: Adicionar log nessa funcao
     UserStorage.setUsername(user);
     UserStorage.setPassword(pass);
     await HttpConnection.dio
@@ -19,14 +15,25 @@ class ServiceLogin {
 
     Response response = await HttpConnection.dio.get(
         'https://sigaa.ufrrj.br/sigaa/logar.do;jsessionid=$_jsessionId?dispatch=logOn&user.login=$user&user.senha=$pass');
+    // print("Status: " + response.statusCode.toString() + " " + response.statusMessage);
+    // print("Headers: " + response.headers.toString());
+    // print("Data: " + response.data);
+
     var url;
     while (response.statusCode == 302) {
-      url = extraiHtml(response);
-      while (url.toString().contains('telaAvisoLogon.jsf')) {
-        await trataTelaAviso(url).then((value) => url = value);
+      if (response.headers.toString().contains("location:")) {
+        var loc = response.headers['location'].toString().split(':');
+        loc[0] = "https";
+        loc[1] = loc[1].substring(0, loc[1].length - 1);
+        url = loc.join(':');
+        print(url.toString());
       }
       response = await HttpConnection.dio.get(url);
+      // print("Status: " + response.statusCode.toString() + " " + response.statusMessage);
+      // print("Headers: " + response.headers.toString());
+      // print("Data: " + response.data);
     }
+    // Handle login error
     if (response.data.toString().contains('rio e/ou senha inv')) {
       print("ERRO AO LOGAR");
       UserStorage.setLoggedIn(false);
@@ -36,70 +43,5 @@ class ServiceLogin {
     UserStorage.setLoggedIn(true);
     appController.instance.setIsLogged(true);
     return true;
-  }
-
-  static Future<String> trataTelaAviso(url) async {
-    Response response = await HttpConnection.dio.get(url);
-    var post = {};
-    if (response.data != null) {
-      var document = parse(response.data);
-      var forms = document.querySelectorAll('form[id*="j_id"]');
-      var form = forms[0];
-      form.attributes.forEach((key, value) {
-        if (key == 'action') {
-          post['action'] = 'https://sigaa.ufrrj.br${value}';
-        }
-      });
-
-      var postData = {};
-      form.children.forEach((element) {
-        if (element.localName == 'input') {
-          var name;
-          var valor;
-          element.attributes.forEach((key, value) {
-            if (key == 'name') {
-              name = value;
-            }
-            if (key == 'value') {
-              valor = value;
-            }
-          });
-          postData[name] = valor;
-        }
-        if (element.localName == 'div') {
-          element.children.forEach((e) {
-            if (e.localName == 'input') {
-              var name;
-              var valor;
-              e.attributes.forEach((key, value) {
-                if (key == 'name') {
-                  name = value;
-                }
-                if (key == 'value') {
-                  valor = value;
-                }
-              });
-              postData[name] = valor;
-            }
-          });
-        }
-      });
-      post['form'] = postData;
-    }
-    log(post.toString());
-    Response respPost =
-        await HttpConnection.dio.post(post['action'], data: post['form']);
-    return extraiHtml(respPost);
-  }
-
-  static extraiHtml(response) {
-    var url;
-    if (response.headers.toString().contains("location:")) {
-      var loc = response.headers['location'].toString().split(':');
-      loc[0] = "https";
-      loc[1] = loc[1].substring(0, loc[1].length - 1);
-      url = loc.join(':');
-    }
-    return url;
   }
 }
--- lib/screens/Login.dart
+++ lib/screens/Login.dart
@@ -1,126 +1,135 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_mobx/flutter_mobx.dart';
-import 'package:mobx/mobx.dart';
+import 'package:rural_de_bolso/model/Aluno.dart';
 import 'package:rural_de_bolso/scrapping/LandingPage.dart';
-import 'package:rural_de_bolso/store/AuthStore.dart';
+import 'package:rural_de_bolso/scrapping/ServiceLogin.dart';
 import 'package:rural_de_bolso/utils/app_router.dart';
+import 'package:rural_de_bolso/components/snackBar.dart';
 
-class Login extends StatefulWidget {
-  @override
-  _LoginState createState() => _LoginState();
-}
-
-class _LoginState extends State<Login> {
-  AuthStore authStore = AuthStore();
-
-  @override
-  void didChangeDependencies() {
-    // TODO: Adicionar log nessa funcao
-    super.didChangeDependencies();
-    reaction((_) => authStore.isLogged, (isLogged) {
-      if (isLogged == true) {
-        LandingPage.instance.extraiInformacaoesLanding().then((value) => {
-              ScaffoldMessenger.of(context).hideCurrentSnackBar(),
-              Navigator.of(context)
-                  .pushReplacementNamed(AppRouter.HOME, arguments: value)
-            });
-      }
-    });
-  }
-
+class Login extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Stack(children: [
-      SizedBox(
-          height: MediaQuery.of(context).size.height,
-          child: Image.asset(
-            'assets/images/bg.jpg',
-            fit: BoxFit.cover,
-          )),
-      SingleChildScrollView(
-        child: Container(
-          width: MediaQuery.of(context).size.width,
-          height: MediaQuery.of(context).size.height,
-          padding: EdgeInsets.fromLTRB(40, 0, 40, 0),
-          child: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Text('Credenciais do SIGAA:',
-                    style: TextStyle(
-                      fontSize: 27,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.green,
-                      shadows: [
-                        Shadow(
-                          color: Colors.black.withOpacity(0.7),
-                          offset: Offset(2, 2),
-                          blurRadius: 5,
-                        )
+      body: Stack(children: [
+        SizedBox(
+            height: MediaQuery.of(context).size.height,
+            child: Image.asset(
+              'assets/images/bg.jpg',
+              fit: BoxFit.cover,
+            )),
+        _body(),
+      ]),
+    );
+  }
+}
+
+class _body extends StatefulWidget {
+  @override
+  __bodyState createState() => __bodyState();
+}
+
+class __bodyState extends State<_body> {
+  String _username = '';
+
+  String _pass = '';
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      child: Container(
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        padding: EdgeInsets.fromLTRB(40, 0, 40, 0),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text('Credenciais do SIGAA:',
+                  style: TextStyle(
+                    fontSize: 27,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.green,
+                    shadows: [
+                      Shadow(
+                        color: Colors.black.withOpacity(0.7),
+                        offset: Offset(2, 2),
+                        blurRadius: 5,
+                      )
+                    ],
+                  )),
+              SizedBox(height: 10),
+              SizedBox(
+                width: 600,
+                child: TextField(
+                  onChanged: (text) {
+                    _username = text;
+                  },
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(10)),
+                    ),
+                    labelText: 'Usuário',
+                    filled: true,
+                    fillColor: Colors.white70,
+                    focusColor: Colors.greenAccent,
+                  ),
+                ),
+              ),
+              SizedBox(height: 10),
+              SizedBox(
+                width: 600,
+                child: TextField(
+                  onChanged: (text) {
+                    _pass = text;
+                  },
+                  obscureText: true,
+                  decoration: InputDecoration(
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(10)),
+                      ),
+                      labelText: 'Senha',
+                      filled: true,
+                      fillColor: Colors.white70),
+                ),
+              ),
+              SizedBox(height: 10),
+              ElevatedButton(
+                onPressed: () async {
+                  ScaffoldMessenger.of(context).showSnackBar(new SnackBar(
+                    content: new Row(
+                      children: <Widget>[
+                        new CircularProgressIndicator(),
+                        new Text("  Signing-In...")
                       ],
-                    )),
-                SizedBox(height: 10),
-                Observer(
-                    name: 'usuario',
-                    builder: (_) {
-                      return SizedBox(
-                        width: 600,
-                        child: TextField(
-                          onChanged: authStore.setUsername,
-                          decoration: InputDecoration(
-                            border: OutlineInputBorder(
-                              borderRadius:
-                                  BorderRadius.all(Radius.circular(10)),
-                            ),
-                            labelText: 'Usuário',
-                            filled: true,
-                            fillColor: Colors.white70,
-                            focusColor: Colors.greenAccent,
-                          ),
-                        ),
-                      );
-                    }),
-                SizedBox(height: 10),
-                Observer(
-                    name: 'senha',
-                    builder: (_) {
-                      return SizedBox(
-                        width: 600,
-                        child: TextField(
-                          onChanged: authStore.setPassword,
-                          obscureText: true,
-                          decoration: InputDecoration(
-                              border: OutlineInputBorder(
-                                  borderRadius:
-                                      BorderRadius.all(Radius.circular(10))),
-                              labelText: 'Senha',
-                              filled: true,
-                              fillColor: Colors.white70),
-                        ),
-                      );
-                    }),
-                SizedBox(height: 10),
-                Observer(builder: (_) {
-                  return ElevatedButton(
-                    style: ElevatedButton.styleFrom(fixedSize: Size(240, 50)),
-                    onPressed: authStore.isValid
-                        ? () {
-                            authStore.login();
+                    ),
+                    duration: new Duration(seconds: 10),
+                  ));
+                  ServiceLogin.doLogin(_username, _pass).then((logado) => {
+                        if (logado)
+                          {
+                            LandingPage.instance
+                                .extraiInformacaoesLanding()
+                                .then((value) => {
+                                      ScaffoldMessenger.of(context)
+                                          .hideCurrentSnackBar(),
+                                      Navigator.of(context)
+                                          .pushReplacementNamed(AppRouter.HOME,
+                                              arguments: value)
+                                    })
                           }
-                        : null,
-                    child: authStore.loading
-                        ? CircularProgressIndicator(
-                            valueColor: AlwaysStoppedAnimation(Colors.white),
-                          )
-                        : Text('Entrar', style: TextStyle(fontSize: 20)),
-                  );
-                }),
-              ],
-            ),
+                        else
+                          {
+                            ScaffoldMessenger.of(context).showSnackBar(
+                                new CustomSnackBar('Usuário ou senha inválidos')
+                                    .getSnackBar())
+                          }
+                      });
+                },
+                child: Text('Entrar', style: TextStyle(fontSize: 20)),
+              ),
+            ],
           ),
         ),
-      )
-    ]));
+      ),
+    );
   }
 }
